93f5d28ea1b77f1bae28c4e077368e8d13323cca
kAleks12 <kacper4553@gmail.com>
2023-01-17 12:21:01 +0100

    Add javafx support to pom.xml

--- src/main/java/org/wust/carshop/Main.java
+++ src/main/java/org/wust/carshop/Main.java
@@ -10,18 +10,26 @@ import org.wust.carshop.service.*;
 import org.wust.carshop.util.PartPair;
 
 import java.util.List;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main extends Application {
 
-        var ls = new LoggingService();
-        var handler = ls.validateRole("postgres", "docker");
-        var id = ls.validateMechanic("Houltham", handler);
-
-        var us = new UtilsService(handler);
-        var ms = new MechanicService(handler, us, id);
-
-        ms.getActiveRepairs().forEach(System.out::println);
-        //Update permissions
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
