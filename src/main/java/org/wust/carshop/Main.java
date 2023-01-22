c4ddaa6b3604b6f71cd75ec32ea3ce7a6dfef516
kAleks12 <kacper4553@gmail.com>
2023-01-22 21:55:45 +0100

    Remove javafx from the project

--- src/main/java/org/wust/carshop/Main.java
+++ src/main/java/org/wust/carshop/Main.java
@@ -1,35 +1,19 @@
 package org.wust.carshop;
 
-import org.jdbi.v3.core.Jdbi;
-import org.postgresql.core.Utils;
-import org.postgresql.ds.PGSimpleDataSource;
-import org.wust.carshop.model.Address;
-import org.wust.carshop.model.Employee;
-import org.wust.carshop.model.Part;
 import org.wust.carshop.service.*;
-import org.wust.carshop.util.PartPair;
 
-import java.util.List;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch();
+        var ls = new LoggingService();
+        var handler = ls.validateRole("postgres", "docker");
+        var id = ls.validateMechanic("Houltham", handler);
+
+        var us = new UtilsService(handler);
+        var ms = new MechanicService(handler, us, id);
+
+        ms.getActiveRepairs().forEach(System.out::println);
+        //Update permissions
     }
+
 
 }
